21ea85318cc68b26e1a459dd65f227f2ed41a697
Sam-Mobed <masdebom@gmail.com>
2023-03-22 12:53:54 -0400

    have to debug

--- src/App.js
+++ src/App.js
@@ -16,6 +16,13 @@ export const ACTIONS ={
 function reducer(state, {type, payload}){
   switch(type){
     case ACTIONS.ADD_DIGIT:
+      if (state.overwrite){
+        return {
+          ...state, 
+          currentOperand: payload.digit,
+          overwrite: false,
+        };
+      }
       if(payload.digit === "0" && state.currentOperand === "0") return state;
       if(payload.digit === "." && state.currentOperand.includes(".")) return state;
       return {
@@ -32,6 +39,13 @@ function reducer(state, {type, payload}){
       if (state.currentOperand == null && state.previousOperand == null){
         return state;
       }
+
+      if (state.currentOperand == null){
+        return {
+          ...state,
+          operation: payload.operation,
+        }
+      }
       //take the current operand and make it the previous one
       if (state.previousOperand == null){
         return {
@@ -39,7 +53,7 @@ function reducer(state, {type, payload}){
           operation : payload.operation,
           previousOperand: state.currentOperand,
           currentOperand: null,
-        }
+        };
       }
 
       //in case there is a previous operand, it does the computation and puts the result.
@@ -51,14 +65,69 @@ function reducer(state, {type, payload}){
       };
 
     case ACTIONS.CLEAR:
-      return {}
+      return {};
+    
+    case ACTIONS.DELETE_DIGIT:
+      if (state.overwrite) {
+        return {
+          ...state,
+          overwrite: false,
+          currentOperand: null
+        }
+      }
+      if (state.currentOperand == null) return state;
+      if (state.currentOperand.length === 1) {
+        return { ...state, currentOperand: null};
+      }
+
+      return {
+        ...state,
+        currentOperand: state.currentOperand.slice(0, -1) //remove the last digit from the current operand. 
+      }
+    case ACTIONS.EVALUATE:
+      if (
+        state.operation == null ||
+        state.currentOperand == null ||
+        state.previousOperand == null
+      ) {
+        return state;
+      } //we just don't have enough enough info to compute, so we don't do anything
+      
+      return{
+        ...state,
+        overwrite: true, //once we're done with an evaluation, next time we click a button it will clear the result. 
+        previousOperand: null,
+        operation: null,
+        currentOperand: evaluate(state),
+      }
   }
 
 } //this function helps manage all of our states for us
 //function reducer(state, action), action is broken up into type and payload, since we know that we will have a number of different actions,
 //different types of actions, and they will pass along parameters. 
 
+function evaluate({ currentOperand, previousOperand, operation}) {
+  const prev = parseFloat(previousOperand);
+  const current = parseFloat(currentOperand);
+  if (isNaN(prev) || isNaN(current)) return ""; //incase there is an issue when converting strings to float.
+  let computation = ""
+  switch (operation){
+    case "+":
+      computation = prev + current;
+      break;
+    case "-":
+      computation = prev - current;
+      break;
+    case "*":
+      computation = prev * current;
+      break;
+    case "÷":
+      computation = prev / current;
+      break;
+  }
+  return computation.toString();
 
+}
 
 function App() {
   const [{currentOperand, previousOperand, operation}, dispatch] = useReducer(reducer, {});
@@ -66,11 +135,11 @@ function App() {
   return ( //we are using css grid here, hence the format of the classnames
     <div className="calculator-grid"> //this div contains the entire calculator
       <div className="output"> //this div contains the black section at the top of the calculator
-        <div className="previous-operand">{previousOperand}{operation}</div> //previous computation, the small one at the top
+        <div className="previous-operand">{previousOperand} {operation}</div> //previous computation, the small one at the top
         <div className="current-operand">{currentOperand}</div> //current computation
       </div>
       <button className="span-two" onClick={() => dispatch({ type: ACTIONS.CLEAR })}>AC</button> //span-two because its larger
-      <button>DEL</button>
+      <button onClick={() => dispatch({ type: ACTIONS.DELETE_DIGIT })}>DEL</button>
       <OperationButton operation="÷" dispatch={dispatch} />
       <DigitButton digit="1" dispatch={dispatch} />//we will use a reducer inside of react that will manage all the different states
       <DigitButton digit="2" dispatch={dispatch} />//different states: current operand previous operand and the operation that we have
@@ -86,7 +155,7 @@ function App() {
       <OperationButton operation="-" dispatch={dispatch} />
       <DigitButton digit="." dispatch={dispatch} />
       <DigitButton digit="0" dispatch={dispatch} />
-      <button className="span-two">=</button>
+      <button className="span-two" onClick={() => dispatch({ type: ACTIONS.EVALUATE })}>=</button>
     </div>
   );
 }
